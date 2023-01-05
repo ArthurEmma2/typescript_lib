2e2e7c2ff22d4a6254887bb971dce75119163252
Arthur Emmanuel <yhawnoworries@gmail.com>
2023-01-05 07:25:29 +0000

    first_commit

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import { Product } from './product.model';
+import 'reflect-metadata';
+import { plainToClass } from 'class-transformer';
+import{validate} from 'class-validator'
+
+
+const products = [
+   {title: 'A carpet', price: 654.999} ,
+    {title: "deos", price: 76}
+]
+
+const newProd = new Product('', -555)
+validate(newProd).then(err =>{
+    if(err.length > 0){
+      alert('there was errors')
+    }
+    console.log(newProd.getInformation())
+})
+
+
+// const p1 = new Product('a book', 12.99)
+
+// const loadedProducts = products.map((product) =>{
+// return new Product(product.title, product.price)
+// })
+
+const loadedProducts = plainToClass(Product, products)
+
+for( const Prod of loadedProducts ){
+    console.log(Prod.getInformation())
+}
+
+// console.log(p1)
